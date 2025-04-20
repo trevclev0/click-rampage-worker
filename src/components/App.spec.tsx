492a46cac00c02eb7fd77b56d66e09ad0be6bb44
Trevor Durán <trevor.m.duran@gmail.com>
2025-04-20 13:50:55 -0500

    feat: install vitest for unit tests

---
+++ src/components/App.spec.tsx
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+
+
+test('Simple unit test', () => {
+    expect(true).toBe(true)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,4 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-})
+});
